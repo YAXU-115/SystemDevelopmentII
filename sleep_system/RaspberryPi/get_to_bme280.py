e523d7ff72ec52b80f6197a0c60d8601278687e2
YAXU-115_RPi <yamaguchi.sota.main@gmail.com>
2025-07-18 19:34:08 +0900

    fix license in main_branch

--- sleep_system/RaspberryPi/get_to_bme280.py
+++ sleep_system/RaspberryPi/get_to_bme280.py
@@ -5,7 +5,7 @@
 # 
 # MIT License
 # 
-# Copyright (c) 2021 Michiharu Takemoto <takemoto.development@gmail.com>
+# Original work: Copyright (c) 2021 Michiharu Takemoto <takemoto.development@gmail.com>
 # Modifications: Copyright (c) 2025 YAXU-115 <yamaguchi.sota.main@gmail.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
