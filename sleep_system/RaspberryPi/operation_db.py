792fa9002892432e52847c8ec31f90bfd0cdc9b8
YAXU-115_RPi <yamaguchi.sota.main@gmail.com>
2025-07-20 15:07:27 +0900

    change code to display the contents within the function  in ./RaspberryPi/operation_db.py  in main_branch

--- sleep_system/RaspberryPi/operation_db.py
+++ sleep_system/RaspberryPi/operation_db.py
@@ -18,6 +18,13 @@ def _get_db_connection():
         db="sleep_system_db", charset="utf8")
 
 def get_db(tblname): 
+
+    def _print_database(sqldata_list):
+        for data in sqldata_list:
+            print(data)
+        if sqldata_list == []:
+            print(sqldata_list)
+        
     """
     import operation_db
     data = operation_db.get_db("tblname")
@@ -41,6 +48,7 @@ def get_db(tblname):
         print(f"DB取得時にエラーが発生しました: {e}")
     finally:
         connection.close()
+        _print_database(sqldata_list)
         return sqldata_list
 
 def put_data_record(temperature, humidity, pressure, altitude, fan_duty, time):
@@ -56,9 +64,12 @@ def put_data_record(temperature, humidity, pressure, altitude, fan_duty, time):
     try:
         with connection.cursor() as cursor:
             sql = "INSERT INTO sensor_readings(temperature, humidity, pressure, altitude, timestamp) VALUES(%s, %s, %s, %s, %s)"
+            print(sql)
             cursor.execute(sql,(temperature, humidity, pressure, altitude, time))
             sql = "INSERT INTO fan_control_logs(pwm_value, timestamp) VALUES(%s, %s)"
+            print(sql)
             cursor.execute(sql,(fan_duty, time))
+            print(f"DBにdataを送信しました。")
         connection.commit()
     except Exception as e:
         print(f"DB送信時にエラーが発生しました: {e}")
@@ -90,15 +101,15 @@ if __name__ == "__main__":
 
     time = datetime.datetime.now()
 
-    print(get_db("sensor_readings"))
-    print(get_db("fan_control_logs"))
+    get_db("sensor_readings")
+    get_db("fan_control_logs")
 
     put_data_record(temperature=25.6, humidity= 30.0, pressure=1013.3, altitude=100.0, fan_duty=35, time=time)
 
-    print(get_db("sensor_readings"))
-    print(get_db("fan_control_logs"))
+    get_db("sensor_readings")
+    get_db("fan_control_logs")
 
     clear_table("sensor_readings","fan_control_logs")
 
-    print(get_db("sensor_readings"))
-    print(get_db("fan_control_logs"))
+    get_db("sensor_readings")
+    get_db("fan_control_logs")
