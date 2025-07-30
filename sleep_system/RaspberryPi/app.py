3aa3bd06389f9599b9a3e7520091021f92c20421
YAXU-115_RPi <yamaguchi.sota.main@gmail.com>
2025-07-30 10:48:52 +0900

    add feature Sign In, Sign up add file templates/signin.html for Sign In add file templates/signup.html for Sign Up add of UI adjustment function in static/css/style.css add code to implement sign-in and sign-up in app.py add data in data/data.csv in sleep_system/RaspberryPi in main branch

--- sleep_system/RaspberryPi/app.py
+++ sleep_system/RaspberryPi/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
-import datetime, csv, socket
+import datetime, os
 from functools import wraps
 import threading
 import main, config
+from dotenv import load_dotenv
+from pathlib import Path
 
+
+script_dir = Path(__file__).resolve().parent
+env_path = script_dir / '.env'
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
 main_thread = None
 feedback_status = None
+signin_flag = False
+sleep_status = "stop"
 
 def log_access(method):
     """アクセスログ用デコレーター"""
@@ -24,27 +32,30 @@ def log_access(method):
 @app.route("/", methods=["GET"])
 @log_access
 def get_index():
-    global main_thread
-    global sleep_status
-    sleep_status = request.args.get('sleep_status') or request.args.get('status', '')
-    if sleep_status:
-        # ここでsleep_statusを処理するロジックを追加できます
-        # 例: データベースに保存、ログに記録など
-        # 現在はコンソールに出力するだけ
-        if sleep_status.lower() == 'start':
-            print("Sleep mode started.")
-            if main_thread is None or not main_thread.is_alive():
-                main_thread = threading.Thread(target=main.main, daemon=True)
-                main_thread.start()
+    global signin_flag
+    if signin_flag:
+        global main_thread
+        global sleep_status
 
-        elif sleep_status.lower() == 'stop':
-            print("Sleep mode stopped.")
-            main.stop()
-            main_thread = None
-        else:
-            print(f"Unknown sleep status: {sleep_status}")
-    """GETリクエストでのインデックスページ"""
-    return render_template("index.html", sleep_status=sleep_status)
+        sleep_status = request.args.get('sleep_status') or request.args.get('status', '')
+        if sleep_status:
+
+            if sleep_status.lower() == 'start':
+                print("Sleep mode started.")
+                if main_thread is None or not main_thread.is_alive():
+                    main_thread = threading.Thread(target=main.main, daemon=True)
+                    main_thread.start()
+
+            elif sleep_status.lower() == 'stop':
+                print("Sleep mode stopped.")
+                main.stop()
+                main_thread = None
+            else:
+                print(f"Unknown sleep status: {sleep_status}")
+        """GETリクエストでのインデックスページ"""
+        return render_template("index.html", sleep_status=sleep_status)
+    else:
+        return render_template("signin.html")
 
 @app.route("/feedback", methods=["POST"])
 @log_access
@@ -55,15 +66,42 @@ def post_feedback():
     if not feedback_status:
         print("フィードバックステータスが提供されていません。")
     if feedback_status:
-        # ここでfeedback_statusを処理するロジックを追加できます
-        # 例: データベースに保存、ログに記録など
-        # 現在はコンソールに出力するだけ
         feedback_data = feedback_status.lower()
 
         print(f"フィードバックステータス: {feedback_data}")
 
     """POSTリクエストでのインデックスページ"""
     return redirect(url_for('index', sleep_status=sleep_status))
+
+@app.route("/signin", methods=["POST"])
+@log_access
+def post_signin():
+    global signin_flag
+    global sleep_status
+    user_id = request.form.get('user_id', '')
+    user_pw = request.form.get('user_pw', '')
+    certification = __get_user(user_id,user_pw)
+
+    if certification:
+        signin_flag = True
+        return  redirect(url_for('index', sleep_status=sleep_status))
+    else:
+        signin_flag = False
+        return render_template("signin.html")
+
+def __get_user(user_id,user_pw):
+    if user_id == None or user_pw == None:
+        return False
+    else:
+        try:
+            user_id_env=os.getenv('APP_USRE_ID')
+            user_pw_env=os.getenv('APP_USRE_PW')
+            if user_id == user_id_env and user_pw == user_pw_env:
+                return True
+            else:
+                return False
+        except:
+            return False
 
 @app.route("/feedback" , methods=["GET"])
 def feedback():
@@ -75,8 +113,16 @@ def index():
     global sleep_status 
     return render_template("index.html", sleep_status=sleep_status)
 
+@app.route("/signup", methods=["GET"])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/signin", methods=["GET"])
+def signin():
+    return render_template("signin.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
     config.LOG_FILE.close()
     print("Flaskアプリケーションを終了します。")    
     main.stop()
